04230833025dec5758552bad906e8b8e8ba70397
Vitaliy <vsssv@ya.ru>
2021-11-17 22:40:27 +0300

    root commit

--- users/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-from .models import CustomUser
-from .serialiazers import CustomUserModelSerializer
-
-
-class CustomUserModelViewSet(ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserModelSerializer
